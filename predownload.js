47192130814a2f9f4696aaada9b1b266fe118c5e
Ahmad-Kemsan <ahmadkemsan@gmail.com>
2021-01-12 14:19:45 +0530

    Updated mac library path.

--- predownload.js
+++ predownload.js
@@ -38,7 +38,7 @@ async function main() {
 			if (args.buildFromSource == null) {
 				return;
 			}
-			files = ['libs/clang/x86_64/libLexActivator.a'];
+			files = ['libs/clang/universal/libLexActivator.a'];
 			url = '/LexActivator-Static-Mac.zip';
 			break;
 		case 'win32': // windows
