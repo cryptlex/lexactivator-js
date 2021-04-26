fc0404270b32573c291681345a39d3214a6cf82e
Adnan Kamili <adnan.kamili@gmail.com>
2021-04-26 16:42:57 +0530

    updated workflow

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,26 +1,26 @@
 $base_url = "https://dl.cryptlex.com/downloads"
 $lexactivator_version ="v3.14.7"
 
-New-Item -Path "$PSScriptRoot\tmp\windows" -ItemType Directory
+New-Item -Path ".\tmp\windows" -ItemType Directory
 $url = "$base_url/$lexactivator_version/LexActivator-Win.zip"
 Write-Host "Downloading LexActivator library ..."
 Write-Host $url
-$output = "$PSScriptRoot\tmp\windows\LexActivator-Win.zip"
+$output = ".\tmp\windows\LexActivator-Win.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 
-Expand-Archive "$PSScriptRoot\tmp\windows\LexActivator-Win.zip" -DestinationPath "$PSScriptRoot\tmp\windows\"
+Expand-Archive ".\tmp\windows\LexActivator-Win.zip" -DestinationPath ".\tmp\windows\"
 
-Copy-Item -Path "$PSScriptRoot\tmp\windows\LexActivator-Win\libs\vc14\x64\LexActivator.lib" -Destination "$PSScriptRoot\"
+Copy-Item -Path ".\tmp\windows\LexActivator-Win\libs\vc14\x64\LexActivator.lib" -Destination ".\"
 npm i
 node-gyp rebuild --msvs_version=2015
 
-Copy-Item -Path "$PSScriptRoot\build\Release\lexactivator.node" -Destination "$PSScriptRoot\lib\bindings\windows\x64\lexactivator.node"
-Copy-Item -Path "$PSScriptRoot\tmp\windows\LexActivator-Win\libs\vc14\x64\LexActivator.dll" -Destination "$PSScriptRoot\lib\bindings\windows\x64\LexActivator.dll"
+Copy-Item -Path ".\build\Release\lexactivator.node" -Destination ".\lib\bindings\windows\x64\lexactivator.node"
+Copy-Item -Path ".\tmp\windows\LexActivator-Win\libs\vc14\x64\LexActivator.dll" -Destination ".\lib\bindings\windows\x64\LexActivator.dll"
 
-Copy-Item -Path "$PSScriptRoot\tmp\windows\LexActivator-Win\libs\vc14\x86\LexActivator.lib" -Destination "$PSScriptRoot\"
+Copy-Item -Path ".\tmp\windows\LexActivator-Win\libs\vc14\x86\LexActivator.lib" -Destination ".\"
 npm i
 node-gyp rebuild --arch=ia32 --msvs_version=2015
 
-Copy-Item -Path "$PSScriptRoot\build\Release\lexactivator.node" -Destination "$PSScriptRoot\lib\bindings\windows\x32\lexactivator.node"
-Copy-Item -Path "$PSScriptRoot\tmp\windows\LexActivator-Win\libs\vc14\x86\LexActivator.dll" -Destination "$PSScriptRoot\lib\bindings\windows\x32\LexActivator.dll"
+Copy-Item -Path ".\build\Release\lexactivator.node" -Destination ".\lib\bindings\windows\x32\lexactivator.node"
+Copy-Item -Path ".\tmp\windows\LexActivator-Win\libs\vc14\x86\LexActivator.dll" -Destination ".\lib\bindings\windows\x32\LexActivator.dll"
 
