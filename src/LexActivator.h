3d72580d15ce9808fd0023abd16a5002358117df
Ahmad Kemsan <ahmad.kemsan@cryptlex.com>
2021-07-14 14:47:39 +0530

    added setDataDirectory function and status code LA_E_CONTAINER

--- src/LexActivator.h
+++ src/LexActivator.h
@@ -129,6 +129,27 @@ LEXACTIVATOR_API int LA_CC SetProductData(CSTRTYPE productData);
 LEXACTIVATOR_API int LA_CC SetProductId(CSTRTYPE productId, uint32_t flags);
 
 /*
+    FUNCTION: SetDataDirectory()
+
+    PURPOSE: In case you want to change the default directory used by LexActivator to
+    store the activation data on Linux and macOS, this function can be used to
+    set a different directory.
+
+    If you decide to use this function, then it must be called on every start of
+    your program before calling SetProductFile() or SetProductData() function.
+
+    Please ensure that the directory exists and your app has read and write
+    permissions in the directory.
+
+    PARAMETERS:
+    * directoryPath - absolute path of the directory.
+
+    RETURN CODES: LA_OK, LA_E_FILE_PERMISSION
+
+*/
+LEXACTIVATOR_API int LA_CC SetDataDirectory(CSTRTYPE directoryPath);
+
+/*
     FUNCTION: SetCustomDeviceFingerprint()
 
     PURPOSE: In case you don't want to use the LexActivator's advanced
@@ -672,7 +693,7 @@ LEXACTIVATOR_API int LA_CC GenerateOfflineDeactivationRequest(CSTRTYPE filePath)
     of your app.
 
     RETURN CODES: LA_OK, LA_EXPIRED, LA_SUSPENDED, LA_GRACE_PERIOD_OVER, LA_FAIL,
-    LA_E_PRODUCT_ID, LA_E_LICENSE_KEY, LA_E_TIME, LA_E_TIME_MODIFIED
+    LA_E_PRODUCT_ID, LA_E_LICENSE_KEY, LA_E_TIME, LA_E_TIME_MODIFIED, LA_E_MACHINE_FINGERPRINT
 
     NOTE: If application was activated offline using ActivateLicenseOffline() function, you
     may want to set grace period to 0 to ignore grace period.
@@ -690,7 +711,7 @@ LEXACTIVATOR_API int LA_CC IsLicenseGenuine();
     want to skip the server sync.
 
     RETURN CODES: LA_OK, LA_EXPIRED, LA_SUSPENDED, LA_GRACE_PERIOD_OVER, LA_FAIL,
-    LA_E_PRODUCT_ID, LA_E_LICENSE_KEY, LA_E_TIME, LA_E_TIME_MODIFIED
+    LA_E_PRODUCT_ID, LA_E_LICENSE_KEY, LA_E_TIME, LA_E_TIME_MODIFIED, LA_E_MACHINE_FINGERPRINT
 
     NOTE: You may want to set grace period to 0 to ignore grace period.
 */
