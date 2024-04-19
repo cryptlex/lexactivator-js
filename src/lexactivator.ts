90b937bdaff5007a3767895862601afe919e194e
Muneeb Khurshid <muneebkq05@gmail.com>
2024-04-19 17:10:13 +0530

    docs: jsdoc update

--- src/lexactivator.ts
+++ src/lexactivator.ts
@@ -156,9 +156,13 @@ export class LexActivator {
 	 *
 	 * @param {string} productId the unique product id of your application as mentioned on
 	 * the product page in the dashboard.
-	 * @param {PermissionFlags} flag depending upon whether your application requires admin/root
-	 * permissions to run or not, this parameter can have one of the following values:
-	 * LA_SYSTEM, LA_USER, LA_IN_MEMORY
+	 * @param {PermissionFlags} flag depending on your application's requirements, choose one of 
+     * the following values: LA_USER, LA_SYSTEM, LA_IN_MEMORY, LA_ALL_USERS.
+     *  - LA_USER: This flag indicates that the application does not require admin or root permissions to run.
+     *  - LA_SYSTEM: This flag indicates that the application must be run with admin or root permissions.
+     *  - LA_IN_MEMORY: This flag will store activation data in memory. Thus, requires re-activation 
+     *    on every start of the application and should only be used in floating licenses.
+     *  - LA_ALL_USERS: This flag is specifically designed for Windows and should be used for system-wide activations.
 	 * @throws {LexActivatorException}
 	 */
 	static SetProductId(productId: string, flag: typeof PermissionFlags[keyof typeof PermissionFlags]): void {
