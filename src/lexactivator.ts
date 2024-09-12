37a78131cd65dc0ec4bb7851b27d7551deb562c1
muneebkq <muneebkq05@gmail.com>
2024-09-12 17:31:21 +0530

    refactor: jsdoc

--- src/lexactivator.ts
+++ src/lexactivator.ts
@@ -78,7 +78,7 @@ export class OrganizationAddress {
  * @property {number} allowedDeactivations The allowed activations of the license. A value of -1 indicates unlimited number of deactivations.
  * @property {string} key The license key
  * @property {string} type The license type (node-locked or hosted-floating).
- * @property {Array<{key: string, value: string}>} metadata License metadata with view_permission set to "user".
+ * @property {Array<{key: string, value: string}>} metadata The license metadata.
  */
 export class UserLicense {
 	allowedActivations: number;
